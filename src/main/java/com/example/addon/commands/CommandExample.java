6eeb719849b0154afcb0dfaf4fa63ae6317df546
advik555 <141858080+therealrealguy@users.noreply.github.com>
2023-08-13 12:39:41 -0400

    i think i made a command 99% chance it doesent work
    man i hate this

--- src/main/java/com/example/addon/commands/CommandExample.java
+++ src/main/java/com/example/addon/commands/CommandExample.java
@@ -1,8 +1,12 @@
+//this was made by chat gpt it most likely does not work
 package com.example.addon.commands;
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import meteordevelopment.meteorclient.commands.Command;
 import net.minecraft.command.CommandSource;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+import meteordevelopment.meteorclient.utils.player.ChatUtils;
 
 import static com.mojang.brigadier.Command.SINGLE_SUCCESS;
 
@@ -14,8 +18,22 @@ public class CommandExample extends Command {
     @Override
     public void build(LiteralArgumentBuilder<CommandSource> builder) {
         builder.executes(context -> {
-            info("hi");
+            // Send a message to the player who executed the command
+            sendChatMessage(context.getSource(), "Hello, world!");
             return SINGLE_SUCCESS;
         });
+    }
+    
+    private void sendChatMessage(CommandSource source, String message) {
+        source.sendFeedback(new LiteralText(message), false);
+    }
+
+    // Intercept incoming chat messages
+    public static void onChatMessage(Text message) {
+        String messageText = message.getString();
+        if (messageText.equalsIgnoreCase("hi")) {
+            // Send a reply of "hello" to the chat
+            ChatUtils.sendMessage("hello");
+        }
     }
 }
